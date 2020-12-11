e33aefce3d1636d81c44a166231b3af37a75e4d8
Ademayowa adeniyi <ademayowaadeniyi@gmail.com>
2020-12-11 03:47:48 +0100

    changed fonts

--- src/App.js
+++ src/App.js
@@ -7,12 +7,12 @@ import './index.css';
 
 const App = () => {
   return (
-    <div className='app'>
+    <>
       <Hero />
       <Experience />
       <Projects />
       <Footer />
-    </div>
+    </>
   );
 };
 
