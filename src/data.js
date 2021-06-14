070056a65d74ba15b54aaf0b6471071245ff444e
ademayowa <ademayowaadeniyi@gmail.com>
2021-06-14 13:29:41 +0100

    updated experience

--- src/data.js
+++ src/data.js
@@ -1,16 +1,26 @@
 const jobs = [
   {
+    title: 'Front-End Engineer',
+    dates: 'Feburary 2021 - Present',
+    duties: [
+      'Integrating APIs into web applications that runs on the blockchain network',
+      'Building websites & applications using react',
+      'Collaborated with designers & backend engineers when developing responsive layout',
+    ],
+    company: 'Jelurida Africa',
+  },
+  {
     title: 'Web Development Instructor',
     dates: 'May 2019 - November 2020',
     duties: [
       'Taught students how to build responsive layouts with HTML CSS and JavaScript (React & Redux)',
-      'Guide students through the development of their final projects using the MERN stack',
+      'Guided students through the development of their final projects using the MERN stack',
       'Taught and guided students how to build RESTFUL APIs using Node.js Express and MongoDB',
     ],
     company: 'NIIT Limited',
   },
   {
-    title: 'Front-End Developer Intern',
+    title: 'Front-End Engineer',
     dates: 'August 2017 - January 2018',
     duties: [
       'Built new user interface using various CSS frameworks',
